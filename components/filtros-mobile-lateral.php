b99fcf9c08593982aa0dcd390905089a52921495
Pedro Henrique <contato@sitesgeniais.com.br>
2025-08-12 13:32:18 -0300

    teste path

--- components/filtros-mobile-lateral.php
+++ components/filtros-mobile-lateral.php
@@ -256,7 +256,7 @@ $impostos = get_terms([
                     <div class="inline-flex items-center text-sm text-blue-600">
                         <svg class="animate-spin -ml-1 mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4 zm2 5.291 A7.962 7.962 0 0,1 4,12 H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" /></path>
                         </svg>
                         Aplicando filtros...
                     </div>
