8d2080e40d3d5acf7cab55cc3b685c93382e8a3e
Pedro Henrique <contato@sitesgeniais.com.br>
2025-08-20 08:25:04 -0300

    Update dashboard.php

--- components/painel/investidor/dashboard.php
+++ components/painel/investidor/dashboard.php
@@ -899,7 +899,7 @@ $investimentos_disponiveis_extrato = get_posts([
                                         <template x-if="movimento.investment_id">
                                             <a :href="`?secao=meus-investimentos&detalhe=${movimento.investment_id}`"
                                                 class="inline-flex items-center px-3 py-1.5 bg-primary text-white text-xs font-medium rounded-lg hover:bg-slate-950 transition-colors">
-                                                <i class="fas fa-eye"></i>Ver detalhes
+                                                <i class="fas fa-eye"></i>
                                             </a>
                                         </template>
                                         <template x-if="!movimento.investment_id">
